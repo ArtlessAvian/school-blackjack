ae7e2b7e8affab9723ae8a0d9f23a95d7486a082
ArtlessAvian <ArtlessAvian@users.noreply.github.com>
2017-01-20 11:37:06 -0800

    Graphics Basics

--- BlackJackVisualize.java
+++ BlackJackVisualize.java
@@ -6,26 +6,64 @@
  * @version (1/17/17)
  */
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Graphics2D;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.lang.Math;
+import java.util.ArrayList;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.Timer;
 
 public class BlackJackVisualize extends JComponent
 {
     BlackJack game;
-    
+    Rectangle r;
+    ArrayList<Card> cardsToDraw;
+
     public BlackJackVisualize()
     {
         game = new BlackJack();
+        game.newRound();
+
+        r = new Rectangle();
+
+        cardsToDraw = new ArrayList<Card>();
     }
-    
-    Rectangle r = new Rectangle();
-    
+
     public void paint(Graphics g)
     {
         Graphics2D g2 = (Graphics2D)g;
+
+        // Draw Background
         r.setSize(WIDTH, HEIGHT);
+        r.x = 0;
+        r.y = 0;
         g2.setColor(Color.getHSBColor(2/3f, 0.5f, 0.5f));
         g2.fill(r);
+
+        // g2.setColor(Color.getHSBColor(0, 1, 1));
+        // r.setSize(100,100);
+        // r.x = (int)(Math.random() * 100);
+        // r.y = (int)(Math.random() * 100);
+        // g2.fill(r);
+
+        //g2.drawImage(cardsToDraw.get(0).img, 0, 0, 100, 200, null);
+
+        // Draw each card
+        for (int i = 0; i < game.currentHand.cards.size(); i++)
+        {
+            Card c = game.currentHand.cards.get(i);
+            if (!cardsToDraw.contains(c))
+            {
+                c.reset();
+                c.posX = i * 100;
+            }
+            c.drawSelf(g2, r);
+        }
     }
 
     final static int WIDTH = 1200;
@@ -36,8 +74,21 @@ public class BlackJackVisualize extends JComponent
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(WIDTH, HEIGHT);
-        frame.setVisible(true);
-        
+
         frame.add(new BlackJackVisualize());
+
+        frame.setVisible(true);        
+
+        class TimerListener implements ActionListener
+        {
+            public void actionPerformed(ActionEvent event)
+            {
+                frame.repaint();
+            }
+        }
+
+        ActionListener listener = new TimerListener();
+        Timer t = new Timer(1000/60, listener);
+        t.start();
     }
 }
--- Card.java
+++ Card.java
@@ -5,46 +5,124 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
 public class Card
 {
-    static String[] facesName = {"Spades", "Clubs", "Diamonds", "Hearts"};
-    static String[] valuesName = {"Ace", "Two", "Three", "Four", "Five", "Six",
-       "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
-   
-    private static Card[] cards = new Card[52];
-   
-    public static Card[] getCards()
-    {
-       if (cards[0] == null)
-       {
-           for (int i = 0; i < 4; i++)
-           {
-                for (int j = 0; j < 13; j++)
-                {
-                   cards[i * 13 + j] = new Card(valuesName[j] + " of " + facesName[i], j + 1, i);
-                }
-            }
-       }
-       return cards;
-    }
-   
-    final String name;
-    final int value; // literal
-    final int suit; // 0 Spades, 1 Clubs, 2 Diamonds, 3 Hearts
-   
-    public Card(String name, int value, int suit)
-    {
-        this.name = name;
-        this.value = value;
-        this.suit = suit;
-    }
-   
-    public static void main(String[] args)
-    {
-        Card[] cards = getCards();
-        //for (int i = 0; i < 52; i++)
-        //{
-        //    System.out.println(i + " " + cards[i].name);
-        //}
-    }
+	static String[] facesName = {"Spade", "Club", "Diamond", "Heart"};
+	static String[] valuesName = {"Ace", "Two", "Three", "Four", "Five", "Six",
+		"Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+
+	private static Card[] cards = new Card[52];
+
+	public static Card[] getCards()
+	{
+		if (cards[0] == null)
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				for (int j = 0; j < 13; j++)
+				{
+					String numberPart;
+
+					switch (j)
+					{
+						case 0 : {numberPart = "A"; break;}
+						case 10 : {numberPart = "J"; break;}
+						case 11 : {numberPart = "Q"; break;}
+						case 12 : {numberPart = "K"; break;}
+						default : {numberPart = (j + 1) + ""; break;}
+					}
+
+					BufferedImage img = null;
+
+					try
+					{
+						File f = new File("Cards/" +
+							numberPart + "_" + facesName[i].toUpperCase() + ".jpg");
+
+						//System.out.println(f.getAbsolutePath());
+
+						img = ImageIO.read(f);
+					}
+					catch (Exception e)
+					{
+						e.printStackTrace();
+					}
+				  
+					cards[i * 13 + j] = new Card(valuesName[j] + " of " + facesName[i] + "s",
+					j + 1, i,
+					img);
+				}
+			}
+		}
+		return cards;
+	}
+	
+	final String name;
+	final int value; // literal
+	final int suit; // 0 Spades, 1 Clubs, 2 Diamonds, 3 Hearts
+
+	// Draw Stuff;
+	final BufferedImage img;
+	static BufferedImage backSide;
+
+	int posX = 0;
+	int posY = 0;
+	
+	public Card(String name, int value, int suit, BufferedImage img)
+	{
+		this.name = name;
+		this.value = value;
+		this.suit = suit;
+		this.img = img;
+
+		if (backSide == null)
+		{
+			try
+			{
+				File f = new File("Cards/BACK.jpg");
+
+				//System.out.println(f.getAbsolutePath());
+
+				Card.backSide = ImageIO.read(f);
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void reset()
+	{
+		posX = 0;
+		posY = 0;
+	}
+
+	public void drawSelf(Graphics2D g2, Rectangle r)
+	{
+        g2.drawImage(img, posX, posY, 165, 240, null);
+        r.x = posX;
+        r.y = posY;
+        r.width = 165;
+        r.height = 240;
+        g2.setColor(Color.BLACK);
+        g2.draw(r);
+	}
+
+	public static void main(String[] args)
+	{
+		Card[] cards = getCards();
+		//for (int i = 0; i < 52; i++)
+		//{
+		//    System.out.println(i + " " + cards[i].name);
+		//}
+	}
 }
